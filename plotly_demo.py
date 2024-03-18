6600a1487b1598f4dc28a79fad86640a9a3b487a
Lina Blijleven <lina.blijleven@hu.nl>
2024-03-18 18:49:30 +0100

    More materials!

--- plotly_demo.py
+++ plotly_demo.py
@@ -3,12 +3,14 @@ plotly_demo.py
 
 Source: https://plotly.com/python/table-subplots/
 """
+#%% Imports
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 import pandas as pd
 import re
 
+#%% Data
 df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/Mining-BTC-180.csv")
 
 #%% Data pre-processing
@@ -17,6 +19,7 @@ for i, row in enumerate(df["Date"]):
     datetime = p.split(df["Date"][i])[0]
     df.iloc[i, 1] = datetime
 
+#%% Create the specification of the plots and layout
 fig = make_subplots(
     rows=3, cols=1,
     shared_xaxes=True,
@@ -26,6 +29,7 @@ fig = make_subplots(
            [{"type": "scatter"}]]
 )
 
+#%% Add the first plot, which looks at mining revenue
 fig.add_trace(
     go.Scatter(
         x=df["Date"],
@@ -36,6 +40,7 @@ fig.add_trace(
     row=3, col=1
 )
 
+#%% Add the second plot, which looks at hash rate
 fig.add_trace(
     go.Scatter(
         x=df["Date"],
@@ -46,6 +51,7 @@ fig.add_trace(
     row=2, col=1
 )
 
+#%% Add the table to the layout
 fig.add_trace(
     go.Table(
         header=dict(
@@ -61,10 +67,13 @@ fig.add_trace(
     ),
     row=1, col=1
 )
+
+#%% Complete the figure layout
 fig.update_layout(
     height=800,
     showlegend=False,
     title_text="Bitcoin mining stats for 180 days",
 )
 
+#%% Show the figure
 fig.show()
