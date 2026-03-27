b6f33661cf37d485e9705f337df67f10032c3d34
Lina (Selina) Blijleven <12709907+SelinaBlijleven@users.noreply.github.com>
2026-03-27 14:15:32 +0100

    removed info

--- pairplot.py
+++ pairplot.py
@@ -4,8 +4,6 @@ Script to create a pairplot using a standard seaborn dataset.
 Find the available datasets on https://github.com/mwaskom/seaborn-data.
 
 Created on Tue Mar 26 15:54:47 2024
-
-@author: Lina Blijleven <selina.blijleven@code-cafe.nl>
 """
 
 import seaborn as sns
